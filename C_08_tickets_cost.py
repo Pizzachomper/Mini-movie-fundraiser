c037241991512eb9ca3bb22936808c71f8583dec
Zane <100880007+Pizzachomper@users.noreply.github.com>
2024-02-16 13:27:02 +1300

    Add files via upload

---
+++ C_08_tickets_cost.py
@@ -0,0 +1,22 @@
+import pandas
+
+#Dictionarys to hold ticket details
+all_names = ["a" "b" "c" "d" "e"]
+all_ticket_costs = ["7.5" "7.5" "10.50" "10.50" "6.50"]
+surcharge = ["0", "0", "0.53", "0.53", "0"]
+
+mini_movie_dict = {
+    "Name": all_names,
+    "Ticket Price": all_ticket_costs,
+    "Surcharge": surcharge
+}
+
+mini_movie_frame = pandas.DataFrame(mini_movie_dict)
+
+#Calculate the total ticket cost (ticket + surcharge)
+mini_movie_frame['Total'] = mini_movie_frame ['Surcharge'] + mini_movie_frame['Ticket Price']
+
+#Calculate the profit for each ticket
+mini_movie_frame['Profit'] = mini_movie_frame['Ticket Price'] - 5
+
+print(mini_movie_frame)
